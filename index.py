769230ea5c18d6219121bc86b28784e3852318cb
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-08 02:19:15 +0530

    Update

--- index.py
+++ index.py
@@ -2,29 +2,28 @@ import mechanize
 import sys
 from bs4 import BeautifulSoup
 
-userId = sys.argv[1]
+userId   = sys.argv[1]
 password = sys.argv[2]
 
-url = "http://slcm.manipal.edu/loginForm.aspx"
+url           = "http://slcm.manipal.edu/loginForm.aspx"
 browserObject = mechanize.Browser()
-response = browserObject.open(url)
+response      = browserObject.open(url)
+
 browserObject.select_form("form1")
-browserObject.form['txtUserid'] = userId
+browserObject.form['txtUserid']   = userId
 browserObject.form['txtpassword'] = password
-browserObject.method = "POST"
-response = browserObject.submit()
-# url = response.geturl()
-# print url
+browserObject.method              = "POST"
 
-response = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
+response   = browserObject.submit()
+
+response   = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
 gradeSheet = BeautifulSoup(response.read(), "html5lib")
-
-response = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
-academics = BeautifulSoup(response.read(), "html5lib")
 
 sys.stdout = open('GradeSheet.txt', 'w')
 print 'Grade Sheet: \n', gradeSheet
 
+response   = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
+academics  = BeautifulSoup(response.read(), "html5lib")
+
 sys.stdout = open('Academics.txt', 'w')
 print 'Academics: \n', academics
-
