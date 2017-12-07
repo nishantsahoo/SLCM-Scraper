b502e17a5e7e69608059bda2290571c6baeb4a4d
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-07 23:47:34 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,30 @@
+import mechanize
+import sys
+from bs4 import BeautifulSoup
+
+userId = sys.argv[1]
+password = sys.argv[2]
+
+url = "http://slcm.manipal.edu/loginForm.aspx"
+browserObject = mechanize.Browser()
+response = browserObject.open(url)
+browserObject.select_form("form1")
+browserObject.form['txtUserid'] = userId
+browserObject.form['txtpassword'] = password
+browserObject.method = "POST"
+response = browserObject.submit()
+# url = response.geturl()
+# print url
+
+response = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
+gradeSheet = BeautifulSoup(response.read(), "html5lib")
+
+response = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
+academics = BeautifulSoup(response.read(), "html5lib")
+
+sys.stdout = open('GradeSheet.txt', 'w')
+print 'Grade Sheet: \n', gradeSheet
+
+sys.stdout = open('Academics.txt', 'w')
+print 'Academics: \n', academics
+
