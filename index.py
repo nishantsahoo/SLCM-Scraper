d4222a8b004bde9df05b6b2aff0eda88e6e81d67
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-09 04:10:04 +0530

    Update

--- index.py
+++ index.py
@@ -3,20 +3,6 @@ import sys
 from bs4 import BeautifulSoup
 import string
 
-userId   = sys.argv[1]
-password = sys.argv[2]
-
-url           = "http://slcm.manipal.edu/loginForm.aspx"
-browserObject = mechanize.Browser()
-response      = browserObject.open(url)
-
-browserObject.select_form("form1")
-browserObject.form['txtUserid']   = userId
-browserObject.form['txtpassword'] = password
-browserObject.method              = "POST"
-
-response = browserObject.submit()
-
 def getAcademics(academics):
 	sys.stdout = open('InteralMarks.txt', 'w')
 	panelGroup = academics.find('div', attrs={'class': 'panel-group internalMarks'})
@@ -32,16 +18,33 @@ def getAcademics(academics):
 		print '-------------------------------------------'
 
 
-response   = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
-academics  = BeautifulSoup(response.read(), "html5lib")
+def main():
+	userId   = sys.argv[1]
+	password = sys.argv[2]
 
-sys.stdout = open('Academics.txt', 'w')
-print 'Academics: \n', academics
+	url           = "http://slcm.manipal.edu/loginForm.aspx"
+	browserObject = mechanize.Browser()
+	response      = browserObject.open(url)
 
-getAcademics(academics)
+	browserObject.select_form("form1")
+	browserObject.form['txtUserid']   = userId
+	browserObject.form['txtpassword'] = password
+	browserObject.method              = "POST"
 
-response   = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
-gradeSheet = BeautifulSoup(response.read(), "html5lib")
+	response = browserObject.submit()
+	response   = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
+	academics  = BeautifulSoup(response.read(), "html5lib")
 
-sys.stdout = open('GradeSheet.txt', 'w')
-print 'Grade Sheet: \n', gradeSheet
+	sys.stdout = open('Academics.txt', 'w')
+	print 'Academics: \n', academics
+
+	getAcademics(academics)
+
+	response   = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
+	gradeSheet = BeautifulSoup(response.read(), "html5lib")
+
+	sys.stdout = open('GradeSheet.txt', 'w')
+	print 'Grade Sheet: \n', gradeSheet
+
+
+main()
