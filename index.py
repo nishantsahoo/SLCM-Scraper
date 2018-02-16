f0ab6535f2a81bd72291cf3dc1ea835719134a48
Nishant Sahoo <nishantsahoo.in@gmail.com>
2018-02-17 02:47:27 +0530

    Attendance added

--- index.py
+++ index.py
@@ -26,31 +26,45 @@ def getAcademics(academics):
 		print 'Subject Code:', subjectData[0].split('  ')[0][14:]
 		print 'Subject Name:', subjectData[0].split('  ')[1]
 
-		print
+		# print
 
-		internalsData  = each.findAll('table', attrs={'class': 'table table-bordered'})
-
-		sessionalsData = internalsData[0]
-		tableRowList   = sessionalsData.findAll('tr')[1:]
-		print 'Internal Data:'
-		for tr in tableRowList:
-			tdList = tr.findAll('td')
-			print tdList[0].text + ': ' + tdList[2].text + '/' + tdList[1].text
-
-		print
-
-		if len(internalsData)>1:
-			print 'Assignment Data:'
-			assignmentData = internalsData[1]
-			tableRowList   = assignmentData.findAll('tr')[1:]
+		if each.find('table', attrs={'class': 'table table-bordered'}):
+			internalsData  = each.find('table', attrs={'class': 'table table-bordered'})
+			sessionalsData = internalsData
+			
+			tableRowList   = sessionalsData.findAll('tr')[1:]
+			print 'Internal Data:'
 			for tr in tableRowList:
 				tdList = tr.findAll('td')
 				print tdList[0].text + ': ' + tdList[2].text + '/' + tdList[1].text
 
-		print
+			print
 
-		print 'Marks Obtained:', subjectData[2].strip()[-6:].strip()
-		print 'Maximum Marks:', subjectData[3].strip()[-6:].strip()
+		# print 'Assignment Data:'
+		# assignmentData = internalsData[1]
+		# tableRowList   = asignmentData.findAll('tr')[1:]
+		# for tr in tableRowList:
+		# 	tdList = tr.findAll('td')
+		# 	print tdList[0].text + ': ' + tdList[2].text + '/' + tdList[1].text
+
+		# print
+
+		# print 'Marks Obtained:', subjectData[2].strip()[-6:].strip()
+		# print 'Maximum Marks:', subjectData[3].strip()[-6:].strip()
+
+		print '-------------------------------------------'
+
+
+	# Attendance
+
+	sys.stdout = open('DataSets/Attendace.txt', 'w')
+	table_attendance = academics.find('table', attrs={'id':'tblAttendancePercentage'})
+	tbody = table_attendance.find('tbody')
+	tr_list = tbody.findAll('tr')
+	for tr in tr_list:
+		td_list = tr.findAll('td')[1:]
+		for td in td_list:
+			print td.text
 
 		print '-------------------------------------------'
 
@@ -104,16 +118,17 @@ def main():
 	browserObject.method              = "POST"
 
 	response   = browserObject.submit()
+	print response.code # status is always 200 ._.
 	response   = browserObject.open("http://slcm.manipal.edu/Academics.aspx")
 	academics  = BeautifulSoup(response.read(), "html5lib")
-
-	# sys.stdout = open('DataSets/Academics.txt', 'w')
-	# print 'Academics: \n', academics
 
 	getAcademics(academics)  # call of the function getAcademics
 
 	response   = browserObject.open("http://slcm.manipal.edu/GradeSheet.aspx")
 	gradeSheet = BeautifulSoup(response.read(), "html5lib")
+
+	# if browserObject.form["ctl00$ContentPlaceHolder1$ddlSemester"] == ["VI"]:
+	# 	browserObject.form["ctl00$ContentPlaceHolder1$ddlSemester"].value = ["V"]
 	
 	getGradeSheet(gradeSheet)  # call of the function getGradeSheet
 
